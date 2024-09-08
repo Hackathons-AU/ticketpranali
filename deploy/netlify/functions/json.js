ff30e0717ffbe9412c9f85d27a7b0d484c7a8dcd
balascode <balacodewizard@gmail.com>
2024-09-08 19:53:34 +0530

    netlify deploy

--- deploy/netlify/functions/json.js
+++ deploy/netlify/functions/json.js
@@ -1,8 +1,17 @@
-const jsonData = require('./db.json');
+const fs = require('fs');
+const path = require('path');
 
 exports.handler = async function(event, context) {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(jsonData),
-  };
-}
+    const filePath = path.resolve(__dirname, '../db.json'); // Adjust path if necessary
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf8'));
+
+    return {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*', // Allow all origins (adjust for security as needed)
+            'Access-Control-Allow-Methods': 'GET'
+        },
+        body: JSON.stringify(data)
+    };
+};
